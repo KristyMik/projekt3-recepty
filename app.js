567282127f1ee1b8eab5b3ed797701c50c2500fd
KristyMik <mikulcova.kristyna@gmail.com>
2022-05-03 00:46:47 +0200

    zobrazeni receptu

--- app.js
+++ app.js
@@ -17,3 +17,100 @@ recept-hodnoceni, recept-nazev, recept-popis.
 
 6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
 */
+
+
+let nalezeneRecepty = recepty;
+let seznamReceptuElement = document.getElementById('recepty');
+
+
+
+
+
+zobrazSeznamReceptu();
+
+function zobrazSeznamReceptu(index) {
+
+  
+    let seznamReceptu = document.querySelector('#recepty');
+    
+    
+    for(let i = 0; i < recepty.length; i++){
+      let novyRecept = document.createElement('div');
+      novyRecept.className = 'recept'; 
+      novyRecept.id = i;
+      seznamReceptu.appendChild(novyRecept);
+     
+    
+      let obrazek = document.createElement('img');
+      obrazek.className = 'recept-obrazek'; 
+      obrazek.src = recepty[i].img;
+      
+    
+      let receptInfo = document.createElement('div');
+      receptInfo.className = 'recept-info';
+      novyRecept.appendChild(obrazek);
+      novyRecept.appendChild(receptInfo);
+     
+      
+      let receptNazev = document.createElement('h3');
+      receptNazev.innerText = recepty[i].nadpis;
+      receptInfo.appendChild(receptNazev);
+
+       
+
+        
+    }  
+   // let novyRecept = document.querySelector('div')
+    nalezeneRecepty.forEach(vratIndex);
+
+  novyRecept.addEventListener('click', zobrazNahledReceptu(index));   
+    
+
+   
+   
+ }
+
+
+ function vratIndex(recept, index){
+    
+  console.log(recept + ' + ' + index);
+
+  recept.ind = recept.index;
+  
+  return index;
+       
+  }
+  
+
+
+ 
+
+function zobrazNahledReceptu(index) {
+
+   
+ console.log(recepty[index]);
+
+ let kliknutyRecept = recepty[index];
+ let receptFoto = document.getElementById('recept-foto');
+ receptFoto.src = kliknutyRecept.img;
+
+ let receptKategorie = documet.getElementById('recept-kategorie');
+ receptKategorie.textContent = kliknutyRecept.kategorie;
+}
+
+
+let vyhledavaniReceptu = document.getElementById('hledat');
+vyhledavaniReceptu.addEventListener('keydown', () => {
+  najdiRecept();
+
+})
+
+function najdiRecept() {
+ // let vyhledavaniReceptu = document.getElementById('hledat');
+  let nalezeneRecepty = recepty.filter(recept => recept.nadpis.toLocaleLowerCase().includes(vyhledavaniReceptu.value))
+
+zobrazSeznamReceptu(nalezeneRecepty);
+}
+
+
+
